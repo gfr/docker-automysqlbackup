004326edcfed2d669c7484bfa83e4a9415d806dc
gfr <gabriel@freinbichler.net>
2018-10-05 18:52:29 +0200

    fixed syntax errors

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ "${ENABLE_HOURLY -eq 1}" ]; then
+if [ "${ENABLE_HOURLY} -eq 1" ]; then
     exec /usr/local/bin/automysqlbackup_hourly
 else
     exec /usr/local/bin/automysqlbackup
